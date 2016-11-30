114eef0a011a5c3f922563c58b35f6a88c5523ed
Alejandro1397 <megatron_alex@hotmail.com>
2016-11-30 02:32:48 +0000

    actualizacion de base de datos

---
+++ cds.h
@@ -0,0 +1,172 @@
+#define MAX 200
+
+#ifndef cds_h
+#define cds_h
+
+struct cds
+{
+	int canciones;
+	char nombre[200];
+	char genero [200];
+	char album[200];
+	float precio;
+};
+
+typedef struct cds CD;
+int indiceAlArreglo=0;// esta variable global se usara para almacenar el indice del ultimo cd ingresado al arreglo
+CD listaCd[MAX];// declaracion de un arreglo de cds de 200 elementos
+
+
+void insertarCd(CD cd){
+	if (indiceAlArreglo >= 0 && indiceAlArreglo < MAX) { // verificamos que inidice este en los rangos correctos
+        listaCd[indiceAlArreglo]=cd;
+        indiceAlArreglo++;
+    }else{
+        printf("El indice apunta fuera del arreglo, favor de revisar la logica");
+    }
+}
+
+ CD nuevoCd(){
+    CD tmp;
+    printf("Introduce el nombre del artista:");
+    scanf("%*c%[^\n]",tmp.nombre); 
+    printf("Introduce el genero:");
+    scanf("%*c%[^\n]",tmp.genero);
+    printf("Introduce el album:");
+    scanf("%*c%[^\n]",tmp.album);
+    printf("Introduce numero de canciones:");
+    scanf(" %d",&tmp.canciones);   // El espacio antes del %c es para que ignore espacios en blanco
+    printf("Introduce el precio:");
+    scanf(" %f",&tmp.precio);
+    insertarCd(tmp);
+    return tmp;
+}
+
+void imprimeCd(CD cd){
+    printf("**************************\n");
+    printf("Nombre del artista: %s\n",cd.nombre);
+    printf("Genero: %s\n",cd.genero);
+    printf("Nombre del album: %s\n",cd.album);
+    printf("Numero de canciones: %d\n",cd.canciones);
+    printf("precio:%.2f\n",cd.precio);
+    printf("**************************\n");
+}
+
+void imprimirLista(){
+    int j=0;
+    for (j = 0; j < indiceAlArreglo; j++) {
+        imprimeCd(listaCd[j]);
+    }
+}
+
+
+void buscarPorNombreArtista(char *nombre){
+    int j=0;
+    for (j = 0; j < indiceAlArreglo; j++) {
+        if(strcmp(listaCd[j].nombre,nombre) == 0){
+            imprimeCd(listaCd[j]);
+        }
+    }
+}
+
+void buscarPorCanciones(int c){
+    int j=0;
+    for (j = 0; j < indiceAlArreglo; j++) {
+        if(c == listaCd[j].canciones){
+            imprimeCd(listaCd[j]);
+        }
+    }
+}
+
+void calcularPromedioPrecio(){
+    int j=0;
+    int tmp=0;
+    for (j = 0; j < indiceAlArreglo; j++) {
+        tmp= tmp + listaCd[j].precio;
+    }
+    printf("\nEl promedio de las canciones de los albunes en la BD es: %d!",tmp/indiceAlArreglo);
+}
+
+int menu(){
+    int opcion=0;
+    printf("\n----------- Menu para la aplicacion de BD para alumnos ---------\n");
+    printf("(1) Crear lista.\n");
+    printf("(2) Guardar a archivo.\n");
+    printf("(3) Leer desde archivo.\n");
+    printf("(4) Mostrar lista.\n");
+    printf("(5) Agregar Cd. \n");
+    printf("(6) Obtener promedio de precios.\n");
+    printf("(7) Buscar Cd por artista. \n");
+    printf("(8) Buscar Cd por canciones. \n");
+    printf("(9) Eliminar Cd \n");
+    printf("(10)Modificar Cd \n");
+    printf("(0) SALIR\n");
+    printf("\n\nElige una opcion:");
+    scanf("%d",&opcion);
+
+    return opcion;
+}
+
+void grabarRegistros(CD r[], int tam){
+    FILE *ptrF;
+
+    if((ptrF=fopen("Cds.dat","w"))==NULL){
+        printf("el archivo no se puede abrir\n");
+    }else{
+        fwrite(r,sizeof(CD),tam,ptrF);
+    }
+
+    fclose(ptrF);
+}
+
+int registrosEnArchivo()
+{
+	FILE *ptrF;
+	int cont=0;
+	CD tmp;
+	if((ptrF=fopen("Cds.dat","rb"))== NULL)
+	{
+		printf("Error al abrir el archivo\n");
+	}else{
+		while(!feof(ptrF)){
+			if(fread(&tmp,sizeof(CD),1,ptrF))
+			cont++;
+		}
+	}
+	fclose(ptrF);
+	return cont;	
+}
+
+
+void leerRegistros(int tam){
+
+    FILE *ptrF;
+
+    if((ptrF=fopen("Cds.dat","rb"))==NULL){
+        printf("el archivo no se puede abrir\n");
+    }
+    else{
+        //for /*(int i=0;i<tam;i++)*/
+        fread(listaCd,sizeof(CD),tam,ptrF);
+    }
+
+    fclose(ptrF);
+}
+
+void eliminarCd(int indice){
+    int i=indice;
+    if(indice >= 0 && indice <= indiceAlArreglo){
+       imprimeCd(listaCd[indice]);
+       for(i=indice; i<=indiceAlArreglo; i++){
+         listaCd[i]=listaCd[i+1];
+       }
+       indiceAlArreglo--;
+    }else{
+      printf("El indice es incorrecto, intentalo nuevamente");
+    } 
+    
+}
+
+#endif /* alumnos_h */
+
+ 
---
+++ proyecto.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "cds.h"
+
+int main(int argc, const char * argv[]) {
+    int opcion=0;
+    int cantidad=0;
+    int j=0;
+    char nombre[120];
+    int canciones;
+    printf("Programa de Cds de musica!\n");
+
+    do {
+        opcion=menu();
+
+        //Aqui va la logica principal
+        switch (opcion) {
+            case 1:
+                 printf("Crear una nueva lista de Cds\n");
+                 printf("Cuantos Cds quieres capturar?:");
+                 scanf("%d",&cantidad);
+                 for(j=0;j<cantidad;j++){
+                   printf("Capturando datos del Cd %d\n",j+1);
+                   nuevoCd();
+                 }
+                break;
+            case 2:
+                printf("Guardar lista al archivo\n");
+                grabarRegistros(listaCd,indiceAlArreglo);
+                break;
+            case 3:
+                printf("Leer la lista desde el archivo\n");
+                 indiceAlArreglo=registrosEnArchivo();
+                printf("Se encontraron %d registros\n",indiceAlArreglo);
+        		//Lectura del arreglo en memoria
+                leerRegistros(indiceAlArreglo);
+                break;
+            case 4:
+                printf("Mostrar todos los datos de la lista\n");
+                imprimirLista();
+                break;
+            case 5:
+                printf("Agregar un nuevo Cd a la lista\n");
+                nuevoCd();
+                break;
+            case 6:
+                printf("Calcular el promedio de precios\n");
+                calcularPromedioPrecio();
+                break;
+            case 7:
+                printf("Buscar Cd por artista\n");
+                printf("Introduce el artista a buscar:");
+                scanf("%*c%[^\n]",nombre);
+                buscarPorNombreArtista(nombre);
+                break;
+            case 8:
+                printf("Buscar cd por numero de canciones\n");
+                printf("Introduce el numero de canciones a buscar:");
+                scanf("%d",&canciones);
+                buscarPorCanciones(canciones);
+                break;
+            case 9:
+                printf("Eliminar Cd\n");
+                eliminarCd(indiceAlArreglo-1);
+                break;
+            case 10:
+                printf("Modificar Cd\n");
+                break;
+            default:
+                printf("Opcion no válida\n");
+                break;
+        }
+
+
+    } while (opcion != 0);
+    return 0;
+}
+
